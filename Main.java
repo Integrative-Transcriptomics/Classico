df189b7c936c98835531ccb888c7b4ff5d04c6ff
Katrin Fischer <katrin.fischer@student.uni-tuebingen.de>
2017-10-24 08:59:04 +0000

    Replace Main.java

--- Main.java
+++ Main.java
@@ -6,12 +6,16 @@ package datastructures;
  */
 public class Main {
 	public static void main(String[] args0){
+		if(args0.length == 2) {
 		SNPTable lepraSNPs = new SNPTable(args0[0]);
 		NewickTree lepraTree = new NewickTree(args0[1]);
 		System.out.println(lepraTree);
 		for(Node i : lepraTree.getNodeList()){
 			System.out.println(i.toString());
 		}
-		System.out.println(lepraSNPs);
+		//System.out.println(lepraSNPs);
+		}else {
+			System.err.println("Geben Sie als ersten Dateipfad die SNP-Tabelle an und als zweite eine Newick-Datei");
+		}
 	}
 }
