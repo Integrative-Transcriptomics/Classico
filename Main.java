87738d26ba1e6d833b1f6d80ea347d45d2d1e02d
Katrin Fischer <katrin.fischer@student.uni-tuebingen.de>
2018-02-19 17:56:07 +0000

    Replace Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,8 @@
 package project;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-
-import datastructures.NewickTree;
-import datastructures.Node;
-import datastructures.SNPTable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Katrin Fischer
@@ -18,8 +14,19 @@ public class Main {
 			Project comcla = new Project(args0[0], args0[1], args0[2]);
 			comcla.compute();
 			comcla.getResults();
+			List<Integer> l = new ArrayList<Integer>();
+			l.add(-1);
+			comcla.showPositions(l);
+			comcla.getUniqueSubtreeSNPs(comcla.getFile().createFile("UniqueSubTree.txt"), 9);
+			comcla.treeSNPs(3125);
+			List<Integer> k = new ArrayList<Integer>();
+			k.add(2);
+			k.add(4);
+			k.add(5);
+			k.add(6);
+			comcla.commonSNPs(k);
 		}else {
-			System.err.println("Geben Sie als ersten Dateipfad die SNP-Tabelle an und als zweite eine Newick-Datei und als dritten den Pfad in dem das Ergebnis-Verzeichnis erzeugt werden soll");
+			System.err.println("Geben Sie als ersten Dateipfad die SNP-Tabelle an und als zweiten eine Newick-Datei und als dritten den Pfad in dem das Ergebnis-Verzeichnis erzeugt werden soll");
 		}
 	}
 }
